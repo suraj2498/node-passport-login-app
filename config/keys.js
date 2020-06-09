af41ada2a97a666858547f15b28901b6014d9d71
Suraj K <48143140+suraj2498@users.noreply.github.com>
2020-06-09 12:52:26 -0400

    added tags for mongo URI connection string

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI : 'mongodb+srv://SurajKumar:abcd1234@registration-project-cluster-jlcts.mongodb.net/registration-login-app?retryWrites=true&w=majority' 
+    MongoURI : 'mongodb+srv://<USER>:<PASSWORD>@registration-project-cluster-jlcts.mongodb.net/<DB NAME>?retryWrites=true&w=majority' 
 };
